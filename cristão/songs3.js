ffc1eebdbf07e968df44209f9baefc025f1512b9
Jonas Fabricio <jonas_fab@hotmail.com>
2023-11-23 01:04:02 -0300

    projeto commit

---
+++ cristão/songs3.js
@@ -0,0 +1,31 @@
+export default [
+    {
+      src: "cristão/Leão (Lion - Elevation Worship) _ ADAI Music, Rodrigo Soeiro e Fernando Silva.mp3",
+      name: "Leão (Lion - Elevation Worship) _ ADAI Music, Rodrigo Soeiro e Fernando Silva",
+    },
+    {
+      src: "cristão/Fernandinho _ Único.mp3",
+      name: "Fernandinho _ Único",
+    },
+    {
+      src: "cristão/Juninho Black - Vem.mp3",
+      name: "Juninho Black - Vem",
+    },
+    {
+      src: "cristão/Juninho Black - Primeiro Amor.mp3",
+      name: "Juninho Black - Primeiro Amor",
+    },
+    {
+      src: "cristão/Rodolfo Abrantes _ Um Presente Pro Futuro.mp3",
+      name: "Rodolfo Abrantes _ Um Presente Pro Futuro",
+    },
+    {
+      src: "cristão/Thalles Roberto - Arde Outra Vez.mp3",
+      name: "Thalles Roberto - Arde Outra Vez",
+    },
+    {
+      src: "cristão/Thalles Roberto - Filho Meu.mp3",
+      name: "Thalles Roberto - Filho Meu",
+    },
+
+  ];
---
+++ treinar/script.js
@@ -0,0 +1,75 @@
+const player = document.querySelector("#player");
+const musicName = document.querySelector("#musicName");
+const playPauseButton = document.querySelector("#playPauseButton");
+const prevButton = document.querySelector("#prevButton");
+const nextButton = document.querySelector("#nextButton");
+const currentTime = document.querySelector("#currentTime");
+const duration = document.querySelector("#duration");
+const progressBar = document.querySelector(".progress-bar");
+const progress = document.querySelector(".progress");
+
+import songs2 from "./songs2.js";
+
+const textButtonPlay = "<i class='bx bx-caret-right'></i>";
+const textButtonPause = "<i class='bx bx-pause'></i>";
+
+let index = 0;
+
+prevButton.onclick = () => prevNextMusic("prev");
+nextButton.onclick = () => prevNextMusic();
+
+playPauseButton.onclick = () => playPause();
+
+const playPause = () => {
+  if (player.paused) {
+    player.play();
+    playPauseButton.innerHTML = textButtonPause;
+  } else {
+    player.pause();
+    playPauseButton.innerHTML = textButtonPlay;
+  }
+};
+
+player.ontimeupdate = () => updateTime();
+
+const updateTime = () => {
+  const currentMinutes = Math.floor(player.currentTime / 60);
+  const currentSeconds = Math.floor(player.currentTime % 60);
+  currentTime.textContent = currentMinutes + ":" + formatZero(currentSeconds);
+
+  const durationFormatted = isNaN(player.duration) ? 0 : player.duration;
+  const durationMinutes = Math.floor(durationFormatted / 60);
+  const durationSeconds = Math.floor(durationFormatted % 60);
+  duration.textContent = durationMinutes + ":" + formatZero(durationSeconds);
+
+  const progressWidth = durationFormatted
+    ? (player.currentTime / durationFormatted) * 100
+    : 0;
+
+  progress.style.width = progressWidth + "%";
+};
+
+const formatZero = (n) => (n < 10 ? "0" + n : n);
+
+progressBar.onclick = (e) => {
+  const newTime = (e.offsetX / progressBar.offsetWidth) * player.duration;
+  player.currentTime = newTime;
+};
+
+const prevNextMusic = (type = "next") => {
+  if ((type == "next" && index + 1 === songs2.length) || type === "init") {
+    index = 0;
+  } else if (type == "prev" && index === 0) {
+    index = songs2.length;
+  } else {
+    index = type === "prev" && index ? index - 1 : index + 1;
+  }
+
+  player.src = songs2[index].src;
+  musicName.innerHTML = songs2[index].name;
+  if (type !== "init") playPause();
+
+  updateTime();
+};
+
+prevNextMusic("init");
